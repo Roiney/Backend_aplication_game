5a1cf3d4e990451ead99f0f1d6268314a2a0f84f
Roiney <nei_beal@hotmail.com>
2025-01-21 08:40:02 -0300

    Game seed

--- dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
+++ dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -14,22 +14,26 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
-    private String imUrl;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    @Column(columnDefinition = "TEXT" )
     private String shortDescription;
+    @Column(columnDefinition = "TEXT" )
     private String longDescription;
 
     public Game(){
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataform, String imUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
-        this.imUrl = imUrl;
+        this.score = score;
+        this.platforms = platforms;
+        this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
@@ -58,6 +62,14 @@ public class Game {
         this.year = year;
     }
 
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
     public String getGenre() {
         return genre;
     }
@@ -66,20 +78,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+    public String getplatforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setplatforms(String platforms) {
+        this.platforms = platforms;
     }
 
-    public String getImUrl() {
-        return imUrl;
+    public String getimgUrl() {
+        return imgUrl;
     }
 
-    public void setImUrl(String imUrl) {
-        this.imUrl = imUrl;
+    public void setimgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 
     public String getShortDescription() {
